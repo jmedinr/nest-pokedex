25e4b58a51e0d07ca80a72701cbe1d65a515b3c3
“jmedinr” <juandiego619@gmail.com>
2024-08-15 08:09:31 -0500

    antes de tarea por lotes

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { AxiosInstance } from 'axios';
+import { PokemonResponse } from './interfaces/pokemon-response.interface';
+
+@Injectable()
+export class SeedService {
+  private readonly axios: AxiosInstance;
+  async executeSeed() {
+    const { data } = await this.axios.get<PokemonResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=650',
+    );
+
+    const pokemonToInsert: { name: string; no: number }[] = [];
+    data.results.forEach(({ name, url }) => {
+      const segments = url.split('/');
+      const no: number = +segments[segments.length - 2];
+
+      pokemonToInsert.push({ name, no });
+    });
+  }
+}
